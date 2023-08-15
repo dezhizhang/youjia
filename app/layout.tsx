093c0dac1e1b96bbf063406252423ea8113e1d33
dezhizhang <1541609448@qq.com>
2023-08-15 20:42:00 +0800

    feat:修改图片

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,17 @@
+/*
+ * :file description: 
+ * :name: /youjia/app/layout.tsx
+ * :author: 张德志
+ * :copyright: (c) 2023, Tungee
+ * :date created: 2023-08-15 12:36:38
+ * :last editor: 张德志
+ * :date last edited: 2023-08-15 20:35:29
+ */
 import { Analytics } from "@vercel/analytics/react";
 import { Metadata } from "next";
 import "../styles/globals.css";
 
-let title = "Dream Room Generator";
+let title = "有家";
 let description = "Generate your dream room in seconds.";
 let ogimage = "https://roomgpt-demo.vercel.app/og-image.png";
 let sitename = "roomGPT.io";
