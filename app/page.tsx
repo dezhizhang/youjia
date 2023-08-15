182fb4fa91158e11e334f33ae50d5e1e0c9c7245
dezhizhang <1541609448@qq.com>
2023-08-15 13:06:41 +0800

    feat:更新文案

--- app/page.tsx
+++ app/page.tsx
@@ -1,46 +1,44 @@
+/*
+ * :file description: 
+ * :name: /youjia/app/page.tsx
+ * :author: 张德志
+ * :copyright: (c) 2023, Tungee
+ * :date created: 2023-08-15 12:36:38
+ * :last editor: 张德志
+ * :date last edited: 2023-08-15 13:03:55
+ */
 import Image from "next/image";
 import Link from "next/link";
 import Footer from "../components/Footer";
-import Header from "../components/Header";
+// import Header from "../components/Header";
 import SquigglyLines from "../components/SquigglyLines";
 
 export default function HomePage() {
   return (
     <div className="flex max-w-6xl mx-auto flex-col items-center justify-center py-2 min-h-screen">
-      <Header />
+      {/* <Header /> */}
       <main className="flex flex-1 w-full flex-col items-center justify-center text-center px-4 sm:mt-20 mt-20 background-gradient">
-        <a
-          href="https://vercel.fyi/roomGPT"
-          target="_blank"
-          rel="noreferrer"
-          className="border border-gray-700 rounded-lg py-2 px-4 text-gray-400 text-sm mb-5 transition duration-300 ease-in-out"
-        >
-          Clone and deploy your own with{" "}
-          <span className="text-blue-600">Vercel</span>
-        </a>
         <h1 className="mx-auto max-w-4xl font-display text-5xl font-bold tracking-normal text-gray-300 sm:text-7xl">
-          Generating dream rooms{" "}
           <span className="relative whitespace-nowrap text-blue-600">
             <SquigglyLines />
-            <span className="relative">using AI</span>
+            <span className="relative">使用人工智能</span>
           </span>{" "}
-          for everyone.
+          为每个人打造梦想的房间
         </h1>
         <h2 className="mx-auto mt-12 max-w-xl text-lg sm:text-gray-400  text-gray-500 leading-7">
-          Take a picture of your room and see how your room looks in different
-          themes. 100% free – remodel your room today.
+        拍一张你的房间的照片，看看你的房间在不同主题下的样子。100% 免费 – 立即改造您的房间。
         </h2>
         <Link
           className="bg-blue-600 rounded-xl text-white font-medium px-4 py-3 sm:mt-10 mt-8 hover:bg-blue-500 transition"
           href="/dream"
         >
-          Generate your dream room
+         生成您的梦想房间
         </Link>
         <div className="flex justify-between items-center w-full flex-col sm:mt-10 mt-6">
           <div className="flex flex-col space-y-10 mt-4 mb-16">
             <div className="flex sm:space-x-8 sm:flex-row flex-col">
               <div>
-                <h3 className="mb-1 font-medium text-lg">Original Room</h3>
+                <h3 className="mb-1 font-medium text-lg">原房间</h3>
                 <Image
                   alt="Original photo of a room with roomGPT.io"
                   src="/original-pic.jpg"
@@ -50,7 +48,7 @@ export default function HomePage() {
                 />
               </div>
               <div className="sm:mt-0 mt-8">
-                <h3 className="mb-1 font-medium text-lg">Generated Room</h3>
+                <h3 className="mb-1 font-medium text-lg">生成的房间</h3>
                 <Image
                   alt="Generated photo of a room with roomGPT.io"
                   width={400}
