cc7f7072909c4eee36aa1731769b3a56fcee1185
dezhizhang <1541609448@qq.com>
2023-08-15 22:08:38 +0800

    feat:update

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,24 +5,22 @@
  * :copyright: (c) 2023, Tungee
  * :date created: 2023-08-15 12:36:38
  * :last editor: 张德志
- * :date last edited: 2023-08-15 22:05:20
+ * :date last edited: 2023-08-15 22:08:21
  */
 
 export default function Footer() {
   return (
     <footer style={{textAlign:'center',padding:'10px 0px'}} className="text-center h-16 sm:h-20 w-full sm:pt-2 pt-4 border-t   border-gray-500">
       <div  className="text-gray-300">
-     
         <a
-          href="https://replicate.com/"
+          href="https://beian.miit.gov.cn/"
           target="_blank"
           rel="noreferrer"
-          style={{textAlign:'center'}}
+          style={{textAlign:'center',textDecoration:'none'}}
           className="hover:underline transition hover:text-gray-200"
         >
          CopyRight@2019-2023 晓智科技 黔ICP备20000421号-2
         </a>
-       
       </div>
     </footer>
   );
